242ebc1dc9c6cef6f35beab733ea5b51a0583a9c
unknown <firstrepo-at-490422960321>
2023-03-21 12:47:51 +0530

    third vpc

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,7 @@ resource "aws_vpc" "default_id_vpc" {
 resource "aws_vpc" "default_id_vpc2" {
   cidr_block = "1.3.0.0/16"
 }
+
+resource "aws_vpc" "default_id_vpc3" {
+  cidr_block = "1.4.0.0/16"
+}
